86bed7fb3a81373508bde115c720d3e411d3e0b5
Priyanka Hotchandani <68430517+PriyankaHotchandani@users.noreply.github.com>
2022-02-23 15:01:14 +0530

    files

---
+++ src/components/explore.js
@@ -0,0 +1,136 @@
+import React, { useState } from "react";
+import { TabContent, TabPane, Nav, NavItem, NavLink, Card, Button, CardTitle, CardImg, CardSubtitle, CardText, Row, Col, CardBody, Form, FormGroup, Label, Input, FormText, FormFeedback } from 'reactstrap';
+import classnames from 'classnames';
+import logo from '../images/logo.jpeg';
+import logo1 from '../images/logo1.jpeg';
+import Me from '../images/Me.jpg';
+import Me1 from '../images/Me1.jpg';
+
+const Explore = (props) => {
+  const [activeTab, setActiveTab] = useState('1');
+
+  const toggle = tab => {
+    if(activeTab !== tab) setActiveTab(tab);
+  }
+
+  console.log("ppp",props.requests);
+  return (
+    <div className="container bg explore-main">
+      {/*<div className="row">*/}
+      {/*    <div className="col-12">*/}
+      {/*      <h3 className="exp-heading">Select the criteria from below tabs whom you would like to donate to..</h3>*/}
+      {/*      <hr />*/}
+      {/*    </div>*/}
+      {/*</div>*/}
+      <div className="row">
+      <div className="col-12">
+      <Nav tabs>
+        <NavItem>
+          <NavLink type="button"
+            className={classnames({ active: activeTab === '1' })}
+            onClick={() => { toggle('1'); }}
+          >
+            Peer To Peer
+          </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink type="button"
+              className={classnames({ active: activeTab === '2' })}
+              onClick={() => { toggle('2'); }}
+            >
+              Charity
+            </NavLink>
+          </NavItem>
+        </Nav>
+        <TabContent activeTab={activeTab}>
+          <TabPane tabId="1">
+            <div className="row tabs-main-container">
+            {
+              props.requests.map((request, key) => {
+                let donator
+
+                if(request.owner === request.donator){
+                  console.log("c seller", true);
+                  donator = "No Donator"
+                } else {
+                  donator = request.donator
+                }
+                return(
+                  <Col sm="12" md="6">
+                    <div className="custom-card">
+                      <div className="header-card">
+                        <img width="130px" height="130px" src={ request.image } alt="Card cap" className="card-image"/>
+                        <div className="header-card-content">
+                          <p className="name">Name: { request.name }</p>
+                          <p className="price">Amount: { window.web3.utils.fromWei(request.price.toString(), 'Ether') } Eth</p>
+                            {
+                              !request.donated
+                              ? <Button
+                                  color="info"
+                                  className="donate-btn"
+                                  id={request.id}
+                                  value={request.price}
+                                  onClick={(event) => {
+                                    props.fullFillRequest(event.target.id, event.target.value)
+                                  }}
+                                >
+                              Donate Now</Button>
+                              : null
+                            }
+                        </div>
+                      </div>
+                        <div className="body-card">
+                          <CardText>Story: { request.story }</CardText>
+                        </div>
+                    </div>
+                    </Col>
+                )
+              })
+            }
+            </div>
+          </TabPane>
+          <TabPane tabId="2">
+            <div className="row tabs-main-container">
+              {
+                props.charityRequests.map((request, key) => {
+                  let status
+
+                  if(request.live === false){
+                    console.log("c seller", true);
+                    status = "Closed"
+                  } else {
+                    status = "Live"
+                  }
+
+                  return(
+                    <Col sm="12" md="6">
+                      <div className="custom-card">
+                        <div className="header-card">
+                          <img width="130px" height="130px" src={ request.image } alt="Card cap" className="card-image"/>
+                          <div className="header-card-content">
+                            <p className="name">Id: { request.id.toString() }</p>
+                            <p className="name">Name: { request.name }</p>
+                            <p className="name">Amount: { request.raiseGoal.toString() } $</p>
+                            <p className="name">Amount Raised: { request.ammountRaised.toString() }</p>
+                            <p className="name">Status: { status }</p>
+                            <p> Visit The Requests For Fund Raising</p>
+                          </div>
+                        </div>
+                          <div className="body-card">
+                            <CardText>Story: { request.cause }</CardText>
+                          </div>
+                      </div>
+                      </Col>
+                  )
+                })
+              }
+            </div>
+          </TabPane>
+        </TabContent>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Explore;
---
+++ src/experiment/explore.js
@@ -0,0 +1,285 @@
+import React, { useState } from "react";
+import { TabContent, TabPane, Nav, NavItem, NavLink, Card, Button, CardTitle, CardImg, CardSubtitle, CardText, Row, Col, CardBody } from 'reactstrap';
+import classnames from 'classnames';
+import logo from '../images/logo.jpeg';
+import logo1 from '../images/logo1.jpeg';
+import Me from '../images/Me.jpg';
+import Me1 from '../images/Me1.jpg';
+
+const Explore = () => {
+  const [activeTab, setActiveTab] = useState('1');
+
+  const toggle = tab => {
+    if(activeTab !== tab) setActiveTab(tab);
+  }
+
+  return (
+    <div className="container bg explore-main">
+      {/*<div className="row">*/}
+      {/*    <div className="col-12">*/}
+      {/*      <h3 className="exp-heading">Select the criteria from below tabs whom you would like to donate to..</h3>*/}
+      {/*      <hr />*/}
+      {/*    </div>*/}
+      {/*</div>*/}
+      <div className="row">
+      <div className="col-12">
+        <Nav tabs>
+          <NavItem>
+            <NavLink type="button"
+              className={classnames({ active: activeTab === '1' })}
+              onClick={() => { toggle('1'); }}
+            >
+              <i className="fa fa-graduation-cap fa-lg"></i> Education
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink type="button"
+              className={classnames({ active: activeTab === '2' })}
+              onClick={() => { toggle('2'); }}
+            >
+              <i className="fa fa-futbol-o fa-lg"></i> Sports
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink type="button"
+              className={classnames({ active: activeTab === '3' })}
+              onClick={() => { toggle('3'); }}
+            >
+              <i className="fa fa-outdent fa-lg"></i> Others
+            </NavLink>
+          </NavItem>
+        </Nav>
+        <TabContent activeTab={activeTab}>
+          <TabPane tabId="1">
+            <div className="row tabs-main-container">
+            <Col sm="12" md="6">
+              <div className="custom-card">
+                <div className="header-card">
+                  <img width="130px" height="130px" src={logo} alt="Card cap" className="card-image"/>
+                  <div className="header-card-content">
+                    <p className="name">Special Title Treatment</p>
+                    <p className="price">Card subtitle</p>
+                    <Button color="info" className="donate-btn">
+                      {/*<i className="fa fa-usd fa-lg"></i> */}
+                      Donate Now</Button>
+                  </div>
+                </div>
+                  <div className="body-card">
+                    <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
+                  </div>
+              </div>
+              </Col>
+            <Col sm="12" md="6">
+              <div className="custom-card">
+                <div className="header-card">
+                  <img width="130px" height="130px" src={logo} alt="Card cap" className="card-image"/>
+                  <div className="header-card-content">
+                    <p className="name">Special Title Treatment</p>
+                    <p className="price">Card subtitle</p>
+                    <Button color="info" className="donate-btn">
+                      {/*<i className="fa fa-usd fa-lg"></i> */}
+                      Donate Now</Button>
+                  </div>
+                </div>
+                <div className="body-card">
+                  <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
+                </div>
+              </div>
+            </Col>
+            <Col sm="12" md="6">
+              <div className="custom-card">
+                <div className="header-card">
+                  <img width="130px" height="130px" src={logo} alt="Card cap" className="card-image"/>
+                  <div className="header-card-content">
+                    <p className="name">Special Title Treatment</p>
+                    <p className="price">Card subtitle</p>
+                    <Button color="info" className="donate-btn">
+                      {/*<i className="fa fa-usd fa-lg"></i> */}
+                      Donate Now</Button>
+                  </div>
+                </div>
+                <div className="body-card">
+                  <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
+                </div>
+              </div>
+            </Col>
+            <Col sm="12" md="6">
+              <div className="custom-card">
+                <div className="header-card">
+                  <img width="130px" height="130px" src={logo} alt="Card cap" className="card-image"/>
+                  <div className="header-card-content">
+                    <p className="name">Special Title Treatment</p>
+                    <p className="price">Card subtitle</p>
+                    <Button color="info" className="donate-btn">
+                      {/*<i className="fa fa-usd fa-lg"></i> */}
+                      Donate Now</Button>
+                  </div>
+                </div>
+                <div className="body-card">
+                  <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
+                </div>
+              </div>
+            </Col>
+            <Col sm="12" md="6">
+              <div className="custom-card">
+                <div className="header-card">
+                  <img width="130px" height="130px" src={logo} alt="Card cap" className="card-image"/>
+                  <div className="header-card-content">
+                    <p className="name">Special Title Treatment</p>
+                    <p className="price">Card subtitle</p>
+                    <Button color="info" className="donate-btn">
+                      {/*<i className="fa fa-usd fa-lg"></i> */}
+                      Donate Now</Button>
+                  </div>
+                </div>
+                <div className="body-card">
+                  <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
+                </div>
+              </div>
+            </Col>
+            <Col sm="12" md="6">
+              <div className="custom-card">
+                <div className="header-card">
+                  <img width="130px" height="130px" src={logo} alt="Card cap" className="card-image"/>
+                  <div className="header-card-content">
+                    <p className="name">Special Title Treatment</p>
+                    <p className="price">Card subtitle</p>
+                    <Button color="info" className="donate-btn">
+                      {/*<i className="fa fa-usd fa-lg"></i> */}
+                      Donate Now</Button>
+                  </div>
+                </div>
+                <div className="body-card">
+                  <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
+                </div>
+              </div>
+            </Col>
+            <Col sm="12" md="6">
+              <div className="custom-card">
+                <div className="header-card">
+                  <img width="130px" height="130px" src={logo} alt="Card cap" className="card-image"/>
+                  <div className="header-card-content">
+                    <p className="name">Special Title Treatment</p>
+                    <p className="price">Card subtitle</p>
+                    <Button color="info" className="donate-btn">
+                      {/*<i className="fa fa-usd fa-lg"></i> */}
+                      Donate Now</Button>
+                  </div>
+                </div>
+                <div className="body-card">
+                  <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
+                </div>
+              </div>
+            </Col>
+            <Col sm="12" md="6">
+              <div className="custom-card">
+                <div className="header-card">
+                  <img width="130px" height="130px" src={logo} alt="Card cap" className="card-image"/>
+                  <div className="header-card-content">
+                    <p className="name">Special Title Treatment</p>
+                    <p className="price">Card subtitle</p>
+                    <Button color="info" className="donate-btn">
+                      {/*<i className="fa fa-usd fa-lg"></i> */}
+                      Donate Now</Button>
+                  </div>
+                </div>
+                <div className="body-card">
+                  <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
+                </div>
+              </div>
+            </Col>
+            </div>
+          </TabPane>
+          <TabPane tabId="2">
+            <Row>
+              <Col sm="4">
+                <Card>
+                <CardImg width="100%" height="250px" src={Me} alt="Card image cap" />
+                  <CardBody>
+                    <CardTitle>Special Title Treatment</CardTitle>
+                    <CardSubtitle>Card subtitle</CardSubtitle>
+                    <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
+                    <Button color="info"><i className="fa fa-usd fa-lg"></i> Donate Now</Button>
+                  </CardBody>
+                </Card>
+              </Col>
+              <Col sm="4">
+                <Card>
+                <CardImg width="100%" height="250px" src={logo} alt="Card image cap" />
+                  <CardBody>
+                    <CardTitle>Special Title Treatment</CardTitle>
+                    <CardSubtitle>Card subtitle</CardSubtitle>
+                    <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
+                    <Button color="info"><i className="fa fa-usd fa-lg"></i> Donate Now</Button>
+                  </CardBody>
+                </Card>
+              </Col>
+              <Col sm="4">
+                <Card>
+                <CardImg width="100%" height="250px" src={Me1} alt="Card image cap" />
+                  <CardBody>
+                    <CardTitle>Special Title Treatment</CardTitle>
+                    <CardSubtitle>Card subtitle</CardSubtitle>
+                    <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
+                    <Button color="info"><i className="fa fa-usd fa-lg"></i> Donate Now</Button>
+                  </CardBody>
+                </Card>
+              </Col>
+            </Row>
+          </TabPane>
+          <TabPane tabId="3">
+            <Row>
+              <Col sm="4">
+                <Card>
+                  <CardImg width="100%" height="250px" src={logo} alt="Card image cap" />
+                  <CardBody>
+                    <CardTitle>Special Title Treatment</CardTitle>
+                    <CardSubtitle>Card subtitle</CardSubtitle>
+                    <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
+                    <Button color="info"><i className="fa fa-usd fa-lg"></i> Donate Now</Button>
+                  </CardBody>
+                </Card>
+              </Col>
+              <Col sm="4">
+                <Card>
+                <CardImg width="100%" height="250px" src={Me} alt="Card image cap" />
+                  <CardBody>
+                    <CardTitle>Special Title Treatment</CardTitle>
+                    <CardSubtitle>Card subtitle</CardSubtitle>
+                    <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
+                    <Button color="info"><i className="fa fa-usd fa-lg"></i> Donate Now</Button>
+                  </CardBody>
+                </Card>
+              </Col>
+              <Col sm="4">
+                <Card>
+                <CardImg width="100%" height="250px" src={Me1} alt="Card image cap" />
+                  <CardBody>
+                    <CardTitle>Special Title Treatment</CardTitle>
+                    <CardSubtitle>Card subtitle</CardSubtitle>
+                    <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
+                    <Button color="info"><i className="fa fa-usd fa-lg"></i> Donate Now</Button>
+                  </CardBody>
+                </Card>
+              </Col>
+              <Col sm="4">
+                <Card>
+                <CardImg width="100%" height="250px" src={logo1} alt="Card image cap" />
+                  <CardBody>
+                    <CardTitle>Special Title Treatment</CardTitle>
+                    <CardSubtitle>Card subtitle</CardSubtitle>
+                    <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
+                    <Button color="info"><i className="fa fa-usd fa-lg"></i> Donate Now</Button>
+                  </CardBody>
+                </Card>
+              </Col>
+            </Row>
+          </TabPane>
+        </TabContent>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Explore;
